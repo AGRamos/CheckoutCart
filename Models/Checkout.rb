b3ab9e042d56b58d3bb4ad028dde67a2d0c4b49c
Alejandro Garrido <alejandro.garrido@wiris.com>
2021-03-18 23:22:26 +0100

    Firts commit with all the files we need to launch the APP

---
+++ Models/Checkout.rb
@@ -0,0 +1,42 @@
+class Checkout
+    @@total_price = 0
+    @@checkout_items = []
+    GREEN_TEA_OFFER = "buy-one-get-one-free"
+    STRAWBERRIES_OFFER = "buy-more-than-three"
+
+    def initialize(pricing_rules)
+       @check_pricing_rules = pricing_rules
+    end
+
+    def scan(item)
+       @@checkout_items.push(item)
+    end
+
+    def total()
+        @@checkout_item_codes = "Basket: "
+        @@total_strawberries = 0
+        @@checkout_items.each_with_index do |item, i|
+            @check_pricing_rules.each do |pricing_rule|
+                if (item == pricing_rule['ItemCode'])
+                    
+                    if (GREEN_TEA_OFFER == pricing_rule['EspecialRule'])
+                        (i != (@@checkout_items.length)) ? @@checkout_item_codes += item + ", " : @@checkout_item_codes += item
+                    elsif (STRAWBERRIES_OFFER == pricing_rule['EspecialRule'])
+                        @@total_strawberries += 1
+                    end
+
+                    @@total_price += Float pricing_rule['Price']
+                    (i != (@@checkout_items.length - 1)) ? @@checkout_item_codes += item + ", " : @@checkout_item_codes += item
+                end
+            end
+        end
+        if (@@total_strawberries > 3)
+               @@total_strawberries = @@total_strawberries * 0.5
+               @@total_price -= @@total_strawberries
+        end
+        if @@total_price == 0
+            return "Add a valid argument!"
+        end
+       return @@checkout_item_codes + "\nTotal price expected: £" + (String @@total_price.round(2))
+    end
+end
---
+++ main.rb
@@ -0,0 +1,24 @@
+load "Models/Checkout.rb"
+require "json"
+
+if ARGV.length > 1
+    puts "We need exactly one arguments separated in commas!"
+    exit
+end
+
+if ARGV[0].nil?
+    puts "Add one argument!"
+    exit
+else
+    items = ARGV[0].split(',')
+end
+
+
+file = File.read('./Data/pricing_rules.json')
+pricing_rules = JSON.parse(file)
+
+co = Checkout.new(pricing_rules)
+items.each do |item|
+    co.scan(item)
+end
+puts co.total
